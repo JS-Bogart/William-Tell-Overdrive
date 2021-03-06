24ce34dc24c2b650f73d51de0fec7d450d532480
Bogey <jeffreysbogart@gmail.com>
2021-03-06 12:38:27 -0800

    Add title image

--- src/index.js
+++ src/index.js
@@ -2,40 +2,38 @@ import Game from "./game.js";
 import GameView from "./game_view.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+  
   const gameCanvas = document.getElementsByClassName("game-canvas")[0];
   gameCanvas.width = 1200;
   gameCanvas.height = 700;
-
   const ctx = gameCanvas.getContext("2d");
-
-  const game = new Game(ctx);
-  new GameView(game, ctx).start();
-
   
+  document.getElementById("play-btn").addEventListener("click", () => {
+
+    const game = new Game(ctx);
+    new GameView(game, ctx).start();
+
+    let moveLeft = false;
+    let moveRight = false;
+
+    document.addEventListener("keydown", event => {
+      if (event.code === "ArrowLeft") {
+        moveLeft = true;
+        game.moveBolt(moveLeft, moveRight);
+      } else if (event.code === "ArrowRight") {
+        moveRight = true;
+        game.moveBolt(moveLeft, moveRight);
+      }
+    });
   
-  // Keypress events
-
-  let moveLeft = false;
-  let moveRight = false;
-
-  document.addEventListener("keydown", event => {
-    if (event.code === "ArrowLeft") {
-      moveLeft = true;
-      game.moveBolt(moveLeft, moveRight);
-    } else if (event.code === "ArrowRight") {
-      moveRight = true;
-      game.moveBolt(moveLeft, moveRight);
-    }
+    document.addEventListener("keyup", event => {
+      if (event.code === "ArrowLeft") {
+        moveLeft = false;
+        game.moveBolt(moveLeft, moveRight);
+      } else if (event.code === "ArrowRight") {
+        moveRight = false;
+        game.moveBolt(moveLeft, moveRight);
+      }
+    });
   });
-
-  document.addEventListener("keyup", event => {
-    if (event.code === "ArrowLeft") {
-      moveLeft = false;
-      game.moveBolt(moveLeft, moveRight);
-    } else if (event.code === "ArrowRight") {
-      moveRight = false;
-      game.moveBolt(moveLeft, moveRight);
-    }
-  });
-
 });
